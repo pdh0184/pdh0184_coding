260aa55d687f355b0eba4dc0a32010a7a24be42c
pdh0184 <pdh0184@naver.com>
2022-02-18 09:58:48 +0900

    first commit

--- 나도코딩 GUI/1.create_GUI.py
+++ 나도코딩 GUI/1.create_GUI.py
@@ -3,4 +3,5 @@ from tkinter import *
 root = Tk()
 root.title("Nado_GUI") 
 root.geometry("640x480+150+100")#창 크기 조절 가로x세로 +x좌표 + y좌표
+root.resizable(False,False)#x,y축 값 변경불가(창크기 변경불가)
 root.mainloop()#창이 닫히지 않도록 함
---
+++ 나도코딩 GUI/2.button.py
@@ -0,0 +1,7 @@
+from tkinter import*
+
+root = Tk()
+root.title("나도코딩")
+
+btn1 = Button(root, text="버튼1")
+btn1.pack()#.pack()을 사용해야 버튼이 보임
